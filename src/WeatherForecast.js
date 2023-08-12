9bd186186f18012e514056435ae419089e318491
neg04 <neg.n04@gmail.com>
2023-08-12 18:03:45 +0330

    fixed the map

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -26,6 +26,8 @@ export default function WeatherForecast(props) {
                             <ForecastDay data={dailyForecast} />
                             </div>
                         );
+                        } else {
+                            return null;
                         }
                     })}
             </div>
